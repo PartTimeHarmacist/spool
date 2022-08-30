53e841e2078021a511753508cfca87bfc092edb3
PartTimeHarmacist <harmacistant@icloud.com>
2022-08-29 20:02:49 -0500

    Added placeholder /oauth endpoint for testing purposes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return "Welcome to the initial commit - there's nothing here right now..."
+
+
+@app.route('/oauth')
+def oauth_endpoint():
+    returned_state = request.args.get('state')
+    returned_code = request.args.get('code')
+    returned_error = request.args.get('error')
+
+    if returned_error:
+        return f"Oauth failed due to error: {returned_error}"
+
+    return f"Oauth Endpoint\nState: {returned_state}\nCode: {returned_code}"
 
 
 if __name__ == '__main__':
